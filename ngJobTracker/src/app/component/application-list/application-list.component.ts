fb770ebdc4250c92f8df3c231a984cdb10d68a53
Hopeful89 <brandon.stine89@gmail.com>
2021-08-16 08:11:48 -0600

    filter and search fully functional

--- ngJobTracker/src/app/component/application-list/application-list.component.ts
+++ ngJobTracker/src/app/component/application-list/application-list.component.ts
@@ -11,22 +11,35 @@ import { ApplicationService } from 'src/app/services/application.service';
 export class ApplicationListComponent implements OnInit {
 
   applications: Application[] = [];
-  constructor(private appService:ApplicationService, private router: Router) { }
+  filteredApp: Application[] = [];
   appView: Application[] = [];
+  constructor(private appService:ApplicationService, private router: Router) { }
   search: string = '';
+  filterSearch: string = '';
   ngOnInit(): void {
     this.loadApplications();
   }
 
   searchFilter(event:any){
     this.search = event.target.value;
-    this.appView = this.applications.filter(app => app.name.toUpperCase().includes(this.search.toUpperCase()) || app.description.toUpperCase().includes(this.search.toUpperCase()));
+    this.appView = this.filteredApp.filter(app => app.name.toUpperCase().includes(this.search.toUpperCase()) || app.description.toUpperCase().includes(this.search.toUpperCase()));
+  }
+
+  filterByStatus(event:any){
+    this.filteredApp = this.applications.filter(app => {
+      if(!event.target.value){
+        return app;
+      }
+      return app.status.name === event.target.value
+    });
+    this.appView = this.filteredApp.map(x => x);
   }
 
   loadApplications() {
     this.appService.index().subscribe(
       res=>{
         this.applications = res;
+        this.filteredApp = this.applications.map(x => x)
         this.appView = this.applications.map(x => x);
       },
       error=>{
